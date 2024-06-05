f2b4fd0c33e823b124b11188e2f5605af0ed3be2
areeba8820 <162459943+areeba8820@users.noreply.github.com>
2024-06-05 16:10:48 +0500

    code

---
+++ app.js
@@ -0,0 +1,76 @@
+// let cleanestCities = ["pak","india","london","australia","newyork"]
+// let cityTocheck = "pak";
+// let matchFound = "no";
+
+// for (let i = 0; i <=4; i++){
+//     if(cityTocheck === cleanestCities[2]){
+//         matchFound = "yes";
+//         alert("its one of the cleanest cities");
+
+//     }
+//     else if(matchFound === "no"){
+//         alert("its not on the list");
+//         break;
+//     }
+// }
+
+
+// let cleanestCities = ["pak","india","london","australia","newyork"]
+// let cityTocheck = "london";
+// let matchFound = false;
+
+
+// for (let i = 0; i <= 4; i++){
+//     if(cityTocheck === cleanestCities[i]){
+//         matchFound = true
+//         alert("its one of the cleanest cities");
+//         break;
+//     }
+//     else if(matchFound === false){
+//         alert("its not on the list");
+//                 break;
+//     }
+// }
+
+
+// let cleanestCities = ["pak","india","london","australia","newyork"]
+// let cityTocheck = "london";
+// let matchFound = false;
+
+
+// for (let i = 0; i <= cleanestCities.length; i++){
+//     if(cityTocheck === cleanestCities[i]){
+//         matchFound = true
+//         alert("its one of the cleanest cities");
+//         break;
+//     }
+//     else if(matchFound ===false){
+//         alert("its not on the list");
+//                 break;
+//     }
+// }
+
+
+// for(let a = 1; a <= 5; a++){
+//     for(let b = 1; b <=a; b++){
+// document.write(b + " ")
+//     }
+//     document.write("<br>")
+// }
+
+
+// for(let a = 1; a <= 5; a++){
+//     for(let b = 1; b <=a; b++){
+// document.write(a + " ")
+//     }
+//     document.write("<br>")
+// }
+
+for(let a = 5; a >= 1; a--){
+    for(let b = a; b >=1; b--){
+document.write(* + " ")
+    }
+    document.write("<br>")
+}
+
+
